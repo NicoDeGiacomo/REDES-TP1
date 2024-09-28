68ea36f4f9e9860655615d61f9b7dac8474d1491
valenmdr <vmdr18@gmail.com>
2024-09-28 19:42:19 -0300

    Avance en el Sender con validaciones

--- src/tcp_sack_sender.py
+++ src/tcp_sack_sender.py
@@ -16,14 +16,38 @@ class TCPSAckSender(TCPSAck):
         #self.answer_connection(error, error_code)
         logger.info(f"Starting upload with TCP + SAck protocol to Address: {self.addr}")
         seq_num = self.initial_seq_num
+
         while True:
-            data = self.file.read(1400)
-            header = Header(seq_num, self.file.eof, self.eoc)
-            self.socket.send_message_to(header.get_bytes() + data)
-            timestamp = time.time() + self.timeout
-            self.timestamps[seq_num] = timestamp
-            packet = Packet(header, data)
-            self.window.append(packet)
+            if len(self.window) < self.window_size and not self.file.eof:
+                data = self.file.read(1400)
+                header = Header(seq_num, self.file.eof, self.eoc)
+                packet = Packet(header, data)
+                self.socket.send_message_to(header.get_bytes() + data)
+
+                timestamp = time.time() + self.timeout
+                self.timestamps[seq_num] = timestamp
+                self.window.append(packet)
+
+                seq_num += len(data)
+
+                due_packets = self.get_due_timestamps()
+                for packet in due_packets:
+                    if packet.retries >= self.max_retry:
+                        logger.info(f"Max retries reached for packet with sequence number: {packet.header.seq_num}. Connection lost.")
+                        self.file.close()
+                        super().close()
+                        return False
+
+                    # Retransmitir el paquete
+                    logger.info(f"Retransmitting packet with sequence number: {packet.header.seq_num}")
+                    self.socket.send_message_to(packet.header.get_bytes() + packet.data)
+                    packet.retries += 1
+
+                    # Actualizar el tiempo de retransmisión
+                    self.timestamps[packet.header.seq_num] = time.time() + self.timeout
+
+
+
     
     def get_due_timestamps(self):
         current_time = time.time()
