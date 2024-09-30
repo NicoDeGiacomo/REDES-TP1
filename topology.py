ac025508548470c5dbd031d8bec61eb9a38c2829
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2024-09-30 00:59:29 -0300

    Modified topology.py

--- topology.py
+++ topology.py
@@ -1,11 +1,11 @@
 from mininet.topo import Topo
 from mininet.link import TCLink
 
+
 class CustomTopo(Topo):
     def __init__(self):
         Topo.__init__(self)
-        self.pakcet_loss = 10
-    
+
         # Crear hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
@@ -15,10 +15,14 @@ class CustomTopo(Topo):
         # Crear switch
         s1 = self.addSwitch('s1')
 
-        # Conectar hosts al switch con pérdida de paquetes del 10%
-        self.addLink(h1, s1, cls=TCLink, loss=self.pakcet_loss)
-        self.addLink(h2, s1, cls=TCLink, loss=self.pakcet_loss)
-        self.addLink(h3, s1, cls=TCLink, loss=self.pakcet_loss)
-        self.addLink(h4, s1, cls=TCLink, loss=self.pakcet_loss)
+        # h1 --> s1, packet_loss=10%
+        self.addLink(h1, s1, cls=TCLink, loss=10)
+        # h2 --> s1, delay=5ms
+        self.addLink(h2, s1, cls=TCLink, delay='15ms')
+        # h3 --> s1, packet_loss=5% and delay=5ms
+        self.addLink(h3, s1, cls=TCLink, loss=5, delay='5ms')
+        # h4 --> s1, normal
+        self.addLink(h4, s1, cls=TCLink)
+
 
 topos = {'custom': CustomTopo}
